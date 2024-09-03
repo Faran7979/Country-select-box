2525a67d8c6c77110f7fe46c46961d59a93ff6c6
Faran.sa <fari79amiri@gmail.com>
2024-09-03 13:18:31 +0330

    Add files via upload

---
+++ Assignment 4/120-explain-of-ticket-mini-project/source/js/script.js
@@ -0,0 +1,19 @@
+﻿let countrySelectBox = document.querySelector(".countrySelect");
+let citySelect = document.querySelector(".citySelect");
+let countriesData = {
+  Iran: ["Tabriz", "Mashhad", "Esfahan", "Yazd", "Tehran", "Shiraz"],
+  Turkey: ["Adana", "Ezmir", "Mercin", "Antalia", "Istambul"],
+  US: ["Los Angeles", "Texas", "Washington", "Pennsylvania"]
+};
+countrySelectBox.addEventListener("change", function () {
+  if (countrySelectBox.value === "Please select") {
+    citySelect.innerHTML = "<option>Please Select city</option>";
+  } else {
+    let mainCountryName = countrySelectBox.value;
+    let mainCountryCities = countriesData[mainCountryName];
+    citySelect.innerHTML = " ";
+    mainCountryCities.forEach(function (city) {
+      citySelect.innerHTML += "<option>" + city + "</option>";
+    });
+  }
+});
